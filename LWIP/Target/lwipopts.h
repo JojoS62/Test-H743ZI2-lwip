f8e01d4396f4853a2717b9c8525b8130bc3db980
Johannes Stratmann <sn2010@onlinehome.de>
2023-06-27 11:22:23 +0200

    modify for 32 byte alignement

--- LWIP/Target/lwipopts.h
+++ LWIP/Target/lwipopts.h
@@ -105,8 +105,6 @@
 #define CHECKSUM_CHECK_TCP 0
 /*----- Value in opt.h for CHECKSUM_CHECK_ICMP6: 1 -----*/
 #define CHECKSUM_CHECK_ICMP6 0
-/*----- Default Value for NETIF_DEBUG: LWIP_DBG_OFF ---*/
-#define NETIF_DEBUG LWIP_DBG_ON
 /*----- Default Value for ICMP_DEBUG: LWIP_DBG_OFF ---*/
 #define ICMP_DEBUG LWIP_DBG_ON
 /*----- Default Value for DHCP_DEBUG: LWIP_DBG_OFF ---*/
